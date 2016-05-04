227d1f70cd41ea5cea0874cff7f36b0594d0d9d9
sanksg <sankalp.gaur@gmail.com>
2016-05-04 16:34:10 +0530

    Hitting a block wrt to accuracy

--- fixGender.py
+++ fixGender.py
@@ -16,18 +16,23 @@ def splitDataset(dataList, splitRatio):
   return [trainSet, copy]
 
 
-def get_ngrams(name, ngramLen):
+def get_ngrams(name, ngramLen, wholeWord):
   if len(name) < ngramLen:
     ngramLen = len(name)
-  ngramList = [name]
-#  for n in range(1, ngramLen+1):
-  ngramList.append(name[-ngramLen:len(name)])
-  ngramList = list(set(ngramList))
+  ngramList = []
   
+  if wholeWord:
+    ngramList.append(name)
+  
+  for n in range(1, ngramLen+1):
+    ngramList.append(name[-n:len(name)])
+    ngramList = list(set(ngramList))
+  
+#  print(ngramList)
   return ngramList
 
 
-def get_counts(dataList, ngramLen):
+def get_counts(dataList, ngramLen, wholeWord):
   genIdx = 1
   namIdx = 0
   
@@ -42,7 +47,7 @@ def get_counts(dataList, ngramLen):
     if gender not in condProbs:
       condProbs[gender]={}
     
-    for name in get_ngrams(name, ngramLen):
+    for name in get_ngrams(name, ngramLen, wholeWord):
       # Create the 2nd level dict for dep value
       if name not in condProbs[gender]:
         condProbs[gender][name] = 0
@@ -76,19 +81,20 @@ def get_probs(condProbs, probs):
     
  
   
-def save_probs(data, fn, ngramLen):
+def save_probs(data, fn, ngramLen, wholeWord):
 
   dataList = []
 
   for row in data:
     if len(row) < 10:
       continue
-    dataList.append([row[0], row[9]])
+    if row[9] == "F":
+      dataList.append([row[0], row[9]])
 
 #  (trainSet, testSet) = splitDataset(dataList, 0.67)
 #  print(len(trainSet), len(testSet))
   
-  condProbs, nameProbs = get_counts(dataList, ngramLen)
+  condProbs, nameProbs = get_counts(dataList, ngramLen, wholeWord)
   condProbs, nameProbs = get_probs(condProbs, nameProbs)
   
   with open(fn, 'wb') as pickleFile:
@@ -103,17 +109,17 @@ def load_probs(fn):
   return (condProbs, nameProbs)
 
 
-def is_female(rec, condProbs, nameProbs, gendProbs, ngramLen):
+def is_female(rec, condProbs, nameProbs, gendProbs, ngramLen, wholeWord):
   name,gend = rec
   
   pr_f = 0
   pr_m = 0
   
-  for ngram in get_ngrams(name, ngramLen):
-    if ngram in condProbs['F']:
+  for ngram in get_ngrams(name, ngramLen, wholeWord):
+    if 'F' in condProbs and ngram in condProbs['F']:
       pr_f += math.log(condProbs['F'][ngram])
       
-    if ngram in condProbs['M']:
+    if 'M' in condProbs and ngram in condProbs['M']:
       pr_m = math.log(condProbs['M'][ngram])
 
   if pr_f != 0:
@@ -121,47 +127,57 @@ def is_female(rec, condProbs, nameProbs, gendProbs, ngramLen):
   if pr_m != 0:
     pr_m = math.exp(pr_m + math.log(gendProbs['M']))
   
-  print(pr_f, pr_m)
+#  print(pr_f, pr_m)
   
   if pr_f > pr_m:
-    if gend=='M':
-      
+#    if gend=='M':
 #      print("%s should be F but marked M, with probs, M: %f, F:%f" % (name, pr_m, pr_f))
-      pass
     return 'F'
   else:
-    return gend
+    return 'M'
   
   
 def main():
-    
+  
+  
   flt = FileUtils()
-  data = flt.load_csv('parsedCsvAll.csv')
   
-  ngramLen = 0
+  dataFn = 'parsedCsvAll.csv'
+  outFn = 'parsedCsv_gender.csv'
+  
+  
+  for i in range(0, 1):
+    print ("Run #%d with dataFn=%s, and outFn=%s" %(i, dataFn, outFn))
+    data = flt.load_csv(dataFn)
 
-  prob_fn = 'probabilities.pcl'
-  
-  save_probs(data[1:100], prob_fn, ngramLen)
-  condProbs, nameProbs = load_probs(prob_fn)
-  
-#  pprint(condProbs, width=1)
-#  pprint(nameProbs, width=1)
-  gendProbs = {'F':0.52, 'M':0.48}
-  
+    ngramLen = 0
+    wholeWord = True
+    
+    prob_fn = 'probabilities.pcl'
 
-#  newCsv = open("parsedCsv_gender.csv", 'w', encoding='utf-8', newline='\n')
-#  csvWriter = csv.writer(newCsv, delimiter=',') 
-#  csvWriter.writerow(data[0])
+    save_probs(data[1:], prob_fn, ngramLen, wholeWord)
+    condProbs, nameProbs = load_probs(prob_fn)
 
-  for row in data[1:100]:
-    if len(row) < 10:
-      continue
-    if row[9] == 'M':
-      row[9] = is_female([row[0], row[9]], condProbs, nameProbs, gendProbs, ngramLen)
-      
-#    csvWriter.writerow(row)
+  #  pprint(condProbs, width=1)
+  #  pprint(nameProbs, width=1)
+    gendProbs = {'F':0.52, 'M':0.48}
 
-  
+
+    newCsv = open(outFn, 'w', encoding='utf-8', newline='\n')
+    csvWriter = csv.writer(newCsv, delimiter=',') 
+    csvWriter.writerow(data[0])
+
+    for row in data[1:]:
+      if len(row) < 10:
+        continue
+      if row[9] == 'M':
+        row[9] = is_female([row[0], row[9]], condProbs, nameProbs, gendProbs, ngramLen, wholeWord)
+
+      csvWriter.writerow(row)
+
+    temp = outFn
+    outFn = dataFn
+    dataFn = temp
+    
 if __name__ == "__main__":
   main()
